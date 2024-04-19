3143b22f057783553a2d713b49e497a3d4cb68b8
Elisa Ranjalahy <elisa.ranjalahy@etu.u-paris.fr>
2024-04-19 02:16:07 +0200

    amélioration bdd, base de code pour extraire bdd dans index.ejs

--- database/build_database.sql
+++ database/build_database.sql
@@ -1,5 +1,5 @@
---Construction des tables--
---TABLE CLIENT
+--CREATION des tables--
+
 CREATE TABLE CLIENTS (
     ID SERIAL PRIMARY KEY,
     PRENOM TEXT,
@@ -15,7 +15,8 @@ CREATE TABLE CLIENTS (
 CREATE TABLE CADEAUX ( --affichage dynamique à faire--
     ID_CADEAU SERIAL PRIMARY KEY,
     NOM_CADEAU TEXT,
-    POINTS_CADEAU INTEGER
+    POINTS_CADEAU INTEGER,
+    IMAGE_CADEAU TEXT
 );
 
 CREATE TABLE LISTE_CADEAUX (
@@ -30,6 +31,7 @@ CREATE TABLE PANIER ( --mettre dans un tableau js dynamique--
     POINTS_CADEAU INTEGER
 );
 
+--REMPLISSAGE des tables
 INSERT INTO CLIENTS (
     PRENOM,
     NOM,
@@ -49,4 +51,14 @@ INSERT INTO CLIENTS (
     'grimpette7A',
     'jeanbav@gmail.com',
     'JeanBav'
+);
+
+INSERT INTO CADEAUX (
+    NOM_CADEAU,
+    POINTS_CADEAU,
+    IMAGE_CADEAU
+) VALUES (
+    'mousqueton',
+    5,
+    '/images/images1.jpeg'
 );
