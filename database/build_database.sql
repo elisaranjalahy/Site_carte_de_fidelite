6fb2737795e0bcc6166813b1a288e248118d8c1d
Elisa Ranjalahy <elisa.ranjalahy@etu.u-paris.fr>
2024-04-25 00:22:44 +0200

    remplissage bdd

--- database/build_database.sql
+++ database/build_database.sql
@@ -8,6 +8,7 @@ CREATE TABLE CLIENTS (
     EMAIL TEXT UNIQUE,
     MOT_DE_PASSE TEXT,
     POINTS_CLIENT INTEGER,
+    ANNIVERSAIRE DATE,
     ADMIN BOOLEAN DEFAULT FALSE
 );
 
@@ -36,20 +37,29 @@ INSERT INTO CLIENTS (
     NOM,
     PSEUDO,
     EMAIL,
-    MOT_DE_PASSE
+    MOT_DE_PASSE,
+    POINTS_CLIENT,
+    ANNIVERSAIRE,
+    ADMIN
 ) VALUES (
     'Big',
     'Boss',
     'bigboss',
     'bigboss@gmail.com',
-    'bossbig'
+    'bossbig',
+    010,
+    '2003-06-10',
+    TRUE
 ),
 (
     'Jean',
     'Bav',
     'grimpette7A',
     'jeanbav@gmail.com',
-    'JeanBav'
+    'JeanBav',
+    100,
+    '2000-05-20',
+    FALSE
 );
 
 INSERT INTO CADEAUX (
@@ -57,7 +67,77 @@ INSERT INTO CADEAUX (
     POINTS_CADEAU,
     IMAGE_CADEAU
 ) VALUES (
-    'mousqueton',
+    'Lot de 2 Mousquetons',
     5,
-    '/images/images1.jpeg'
+    '/images/image1.jpeg'
+),
+(
+    'Une séance bloc avec François Civil',
+    350,
+    '/images/image2.jpeg'
+),
+(
+    'Chaussons Scarpa',
+    1000,
+    '/images/image3.jpeg'
+),
+(
+    'Brosse à prise',
+    15,
+    '/images/image4.jpeg'
+),
+(
+    'Carte de 10 entrée à Climb Up',
+    600,
+    '/images/image5.jpeg'
+),
+(
+    'Crashpad SNAP',
+    250,
+    '/images/image6.jpeg'
+),
+(
+    'Magnésie Liquide SNAP',
+    20,
+    '/images/image7.jpeg'
+),
+(
+    'Magnésie en poudre',
+    20,
+    '/images/image8.jpeg'
+),
+(
+    'Sac pour magnésie ARCTERYX',
+    150,
+    '/images/image9.jpeg'
+),
+(
+    'Lot de 4 Strap',
+    15,
+    '/images/image10.jpeg'
+),
+(
+    'T-shirt Patagonia Bleu Marine',
+    25,
+    '/images/image11.jpeg'
+),
+(
+    'Box de 10 prises avec vis',
+    40,
+    '/images/image12.jpeg'
+),
+(
+    'Poutre',
+    120,
+    '/images/image13.jpeg'
+),
+(
+    'Ecouteurs Bluetooth',
+    300,
+    '/images/image14.jpeg'
+),
+(
+    'Lot de 8 chouchous',
+    10,
+    '/images/image15.jpeg'
 );
